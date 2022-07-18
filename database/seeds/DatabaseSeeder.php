de394867b498191ab7a2b2c14544fa483b5f45c7
matte887 <m.erroi92@gmail.com>
2022-07-18 11:50:35 +0200

    Corretti seed nel DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(AthletesTableSeeder::class);
-        $this->call(Categories::class);
-        $this->call(Nations::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(NationsTableSeeder::class);
     }
 }
