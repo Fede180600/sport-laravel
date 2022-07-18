b4cb65334530a36a5ecf9f43055ac984e2a29dec
Fede180600 <fede180600@libero.it>
2022-07-18 10:55:48 +0200

    aggiungere id_nazione a tabella athlets

---
+++ database/migrations/2022_07_18_084541_add_nation_id_to_athletes.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNationIdToAthletes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('athletes', function (Blueprint $table) {
+            $table->unsignedBigInteger('nation_id')->nullable()->after('name');
+            $table->foreign('nation_id')
+            ->references('id')
+            ->on('nations')
+            ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('athletes', function (Blueprint $table) {
+            $table->dropForeign('athletes_nation_id_foreign');
+            $table->dropColumn('nation_id');
+        });
+    }
+}
