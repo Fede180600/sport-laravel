a691f98519dfa31174a8328aabab4935084be83f
matte887 <m.erroi92@gmail.com>
2022-07-18 11:48:42 +0200

    Aggiunti seed nel databaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(AthletesTableSeeder::class);
+        $this->call(Categories::class);
+        $this->call(Nations::class);
     }
 }
